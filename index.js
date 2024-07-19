b42f1724c8bf8520a1d380ccf9168668a3fb49ef
Jorge Paul <jserranopaul@gmail.com>
2024-07-19 19:08:17 +0200

    queries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import express and the db file just for side effects (to connect to the database)
 import express from 'express';
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from './controllers/users.js';
 import './db/index.js';
 // Create an express app
 const app = express();
@@ -8,14 +9,7 @@ const port = process.env.PORT || 8080;
 // This line lets us use the JSON body of a request in our routes as req.body
 app.use(express.json());
 // app.route() helps us define handlers for different HTTP methods on the same route
-app
-  .route('/users')
-  .get((req, res) => res.json({ message: 'GET /' }))
-  .post((req, res) => res.json({ message: 'POST /' }));
-app
-  .route('/users/:id')
-  .get((req, res) => res.json({ message: 'GET /:id' }))
-  .put((req, res) => res.json({ message: 'PUT /:id' }))
-  .delete((req, res) => res.json({ message: 'DELETE /:id' }));
+app.route('/users').get(getUsers).post(createUser);
+app.route('/users/:id').get(getUserById).put(updateUser).delete(deleteUser);
 // Start the server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
